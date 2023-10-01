dd98098fe28c7d4b5d2f7ef7c83e4e50f331bcba
lzc <lzc@lzcdeMacBook-Pro.local>
2023-10-01 10:12:01 +0800

    feat: stash

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-
+import "./App.css";
+import Button from "./components/Button";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        主应用，提供远程模块供子应用消费
-      </header>
+      <header className="App-header">主应用，提供远程模块供子应用消费</header>
+      <div>
+        <Button />
+      </div>
     </div>
   );
 }
---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function Button() {
+  return (
+    <div className="button">
+       <button>主应用：按钮组件</button>
+    </div>
+  );
+}
+
+export default Button;
