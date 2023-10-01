0ee522150e7589a2cebe6eeda14b58d840d43ab3
lzc <lzc@lzcdeMacBook-Pro.local>
2023-10-01 10:01:04 +0800

    feat: stash

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        主应用，提供远程模块供子应用消费
       </header>
     </div>
   );
